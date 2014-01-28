bfafe8ba50659410d336144ca2e7e6e93081324f
Thorwald Lundqvist <thorwald@jumpstarter.io>
2014-01-28 11:43:25 +0100

    checkout our fork of remoto that works with python2

--- vendor.py
+++ vendor.py
@@ -66,10 +66,7 @@ def vendor_library(name, version, cmd=None):
             run(['rm', '-rf', vendor_dest])
 
     if not path.exists(vendor_dest):
-        rc = run(['git', 'clone', 'git://ceph.com/%s' % name])
-        if rc:
-            print "%s: git clone failed using ceph.com url with rc %s, trying github.com" % (path.basename(__file__), rc)
-            run(['git', 'clone', 'https://github.com/ceph/%s.git' % name])
+        run(['git', 'clone', 'https://github.com/jumpstarter-io/%s' % name])
         os.chdir(vendor_src)
         run(['git', 'checkout', version])
         if cmd:
